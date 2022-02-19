e97b00170acc4be5e63e15900ae0f6be529f5a7a
Anjali Sharma <anjali.sharma@kiprosh.com>
2022-02-20 01:32:50 +0530

    added configuration db

--- src/IdentityServer/Startup.cs
+++ src/IdentityServer/Startup.cs
@@ -2,12 +2,15 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using IdentityServerHost.Quickstart.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Linq;
 using System.Reflection;
 
 namespace IdentityServer
@@ -23,10 +26,11 @@ namespace IdentityServer
 
         public void ConfigureServices(IServiceCollection services)
         {
+
             // uncomment, if you want to add an MVC-based UI
             services.AddControllersWithViews();
             var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
-            const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;database=IdentityServer4.Quickstart.EntityFramework-4.0.0;trusted_connection=yes;";
+            const string connectionString = @"Data Source=.\sqlexpress;initial catalog=IdentityServer4;integrated security=True;MultipleActiveResultSets=True;";
             services.AddIdentityServer()
                                   .AddDeveloperSigningCredential() //This is for dev only scenarios when→you don’t have a certificate to use.                                 
                                                                    //.AddInMemoryIdentityResources(Config.IdentityResources)
@@ -48,6 +52,7 @@ namespace IdentityServer
 
         public void Configure(IApplicationBuilder app)
         {
+            InitializeDatabase(app);
             if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -66,5 +71,41 @@ namespace IdentityServer
                 endpoints.MapDefaultControllerRoute();
             });
         }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService < IServiceScopeFactory >().CreateScope())
+{
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+                var context = serviceScope.ServiceProvider.GetRequiredService< ConfigurationDbContext > ();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.IdentityResources)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+                if (!context.ApiScopes.Any())
+                {
+                    foreach (var resource in Config.ApiScopes)
+                    {
+                        context.ApiScopes.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }
+
+
     }
 }
